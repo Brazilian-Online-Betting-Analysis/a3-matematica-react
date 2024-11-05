ec5536c12836f9be9d853b3dc2e3f02d1a9dcdd8
Nicholas Costa <nicholascostadev@gmail.com>
2024-11-05 19:48:28 -0300

    remove not used function

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import {
   createBrowserRouter,
-  redirect,
   redirectDocument,
   RouterProvider,
 } from "react-router-dom";
